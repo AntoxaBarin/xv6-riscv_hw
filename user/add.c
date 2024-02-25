6c489759667309bfc25f81133158c5c2615e61cd
Ivan Shanygin <shanygin101@yandex.ru>
2024-02-25 23:41:39 +0300

    remove extra buffers

--- user/add.c
+++ user/add.c
@@ -16,67 +16,63 @@ int
 main() {
     char buf[BUF_LEN] = "";      
     gets(buf, BUF_LEN);
+    
+    char *number_1;
+    char *number_2;
 
-    char number1[BUF_LEN] = ""; 
-    char number2[BUF_LEN] = "";
-    // pointers to symbols in buffer and number
-    int i = 0, j = 0;             
+    // pointer to symbols in buffer
+    int i = 0;          
 
     if ((buf[0] == '0' && buf[1] != ' ') || (buf[0] == ' ')) {
         write(2, "Incorrect first number!\n", INC_FST_ERR_LEN);
         exit(1);
     }
-               
+    number_1 = buf + i;    // pointer to first number 
+
     // parse first number
     while (buf[i] != ' ') {                         
         if (buf[i] < '0' || buf[i] > '9') {
             write(2, "Incorrect input!\n", INC_INPUT_ERR_LEN);
             exit(1);
         }
-        number1[j++] = buf[i++];
-    
         if (i == (BUF_LEN - 1) && buf[i] != '\0') {
             write(2, "Wrong number of arguments!\n", WR_ARGS_NUM_ERR_LEN);
             exit(1);
         }   
+        i++;
     }
-    if ((j == 0) || (j == 1 && (number1[0] < '0' || number1[0] > '9'))) {
+    if ((i == 0) || (i == 1 && (*number_1 < '0' || *number_1 > '9'))) {
         write(2, "Wrong number of arguments!\n", WR_ARGS_NUM_ERR_LEN);
         exit(1);
     }   
-
-    number1[j] = '\0';
    
-    j = 0;
     i++;       // skip space symbol between numbers
 
     if (buf[i] == '0' && !(buf[1] == '\n' || buf[1] == '\0')) {
         write(2, "Incorrect second number!\n", INC_SND_ERR_LEN);
         exit(1);
     } 
-    
+    number_2 = buf + i; // pointer to second number
+
     // parse second number
     while (buf[i] != '\0') {                        
         if ((buf[i] < '0' || buf[i] > '9') && buf[i] != '\n') {
             write(2, "Incorrect input!\n", INC_INPUT_ERR_LEN);
             exit(1);
         }
-        number2[j++] = buf[i++];
-
         if (i == (BUF_LEN - 1) && buf[i] != '\0') {
             write(2, "Too long input string!\n", LNG_INP_ERR_LEN);
             exit(1);
         }
+        i++;
     }
-    if ((j == 0) || (j == 1 && (number2[0] < '0' || number2[0] > '9'))) {
+    if ((buf + i - number_2 == 0) || (buf + i - number_2 == 1 && (*number_2 < '0' || *number_2 > '9'))) {
         write(2, "Wrong number of arguments!\n", WR_ARGS_NUM_ERR_LEN);
         exit(1);
     }   
 
-    number2[j] = '\0';
-
-    int int_number1 = atoi(number1);
-    int int_number2 = atoi(number2);
+    int int_number1 = atoi(number_1);
+    int int_number2 = atoi(number_2);
 
     printf("%d\n", int_number1 + int_number2);
     exit(0);
